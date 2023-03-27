51eec8a06dc4fb30c0cb732091abe1868ad00525
edake <102189081+edake419@users.noreply.github.com>
2023-03-27 14:16:13 +0000

    arrays_pointers_strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *_strlen -length of a string
+ *@s: A pointer to an int that will be changed/updated
+ *
+ *Return: void that means our answer is correct
+ */
+
+int _strlen(char *s)
+{
+    return (strlen(*s));
+}
