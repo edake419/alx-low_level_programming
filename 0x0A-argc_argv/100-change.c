ff4ab75c2fbc1696d174c72cb8bbef4f4caff024
edake <102189081+edake419@users.noreply.github.com>
2023-04-11 02:05:39 +0000

    extra

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -7,38 +7,37 @@
  *
  * Return: 0 success 1 failure.
  */
-int main(int argc, char **argv)
-{
-	int coins, i, ammount;
-	int value[5] = {25, 10, 5, 2, 1};
 
-	coins = 0;
-	ammount = atoi(argv[argc - 1]);
+int main(int argc, char *argv[])
+{
+	int coins, i, total;
+	int num[] = {25, 10, 5, 2, 1};
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else if (ammount <= 0)
+
+	num = atoi(argv[1]);
+	total = 0;
+
+	if (coins < 0)
 	{
 		printf("0\n");
+		return (0);
 	}
-	else
+
+	for (i = 0; i < 5 && coins >= 0; i++)
 	{
-		for (i = 0; i < 5; i++)
+		while (coins >= num[i])
 		{
-			if (value[i] <= ammount)
-			{
-				coins = coins + (ammount / value[i]);
-				ammount = ammount - (ammount / value[i]) * value[i];
-				if (ammount == 0)
-				{
-					printf("%d\n", coins);
-					break;
-				}
-			}
+			total++;
+			coins -= num[i];
 		}
 	}
+
+	printf("%d\n", total);
 	return (0);
+}
 }
