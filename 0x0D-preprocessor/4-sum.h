4ecd4548e2f359668e39b636e1dd1aed6b8b19b3
edake <edisondake@gmail.com>
2023-04-24 22:41:11 +0000

    sum

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,4 +1,5 @@
-#ifndef SUM(x, y)
+#ifndef _SUM_LIKE_MACRO_
+#define _SUM_LIKE_MACRO_
 
 #define SUM(x, y) ((x) + (y))
 
---
+++ 0x0D-preprocessor/4-sum.h~
@@ -0,0 +1,6 @@
+#ifndef SUM(x, y)
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
+ 
