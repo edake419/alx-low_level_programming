e13fa208e47106d73659c37e56b7f535b8b85e4a
edake <edisondake@gmail.com>
2023-04-24 21:28:40 +0000

    filename

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+*main prints the name from file it compiled from followed by a new line
+*the predifined File macros is used
+*/
+
+int main ()
+{
+	printf("File :%s\n', _FILE_);
+}
