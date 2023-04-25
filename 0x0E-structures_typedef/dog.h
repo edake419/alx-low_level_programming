8766927040f872d893381318f0baa3a9bf09b6cc
edake <edisondake@gmail.com>
2023-04-25 15:52:29 +0000

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
-int _putchar (charc c);
+int _putchar (char c);
+/**
+ * struct dog - data structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: dog with name, age, and owner's name.
+ */
 
 struct dog
 {
