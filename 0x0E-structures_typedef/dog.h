2d995b6e91755b5e355ddc00370e84acf1b07b22
edake <edisondake@gmail.com>
2023-04-25 15:45:11 +0000

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
 
+int _putchar (charc c);
+
 struct dog
 {
 	char *name;
@@ -13,6 +15,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
-
 #endif
