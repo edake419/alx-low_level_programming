8f038c216236d2dea3cbf1d3f5264fa3942cfdb7
edake <edisondake@gmail.com>
2023-04-28 00:37:51 +0000

    addnode

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,23 +14,19 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	unsigned int len = 0;
 
-    	if (!head || !str)
-                return (NULL);
+	while (str[len])
+		len++;
 
 	new_node = malloc(sizeof(list_t));
 	if (!new_node)
 		return (NULL);
 
 	new_node->str = strdup(str);
-	if (!new_node->str)
-        {
-                free(new_node);
-                return (NULL);
-        }
-
+	new_node->len = len;
 	new_node->next = (*head);
-	*head = new_node;
+	(*head) = new_node;
 
 	return (new_node);
 }
