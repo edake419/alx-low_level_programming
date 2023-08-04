e8354d39492eee5b6803d195446c777d30c45804
edake <102189081+edake419@users.noreply.github.com>
2023-08-04 05:37:43 +0000

    bit manipulation

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * flip_bits - Counts the number of bits to flip to get from n to m
+ * @n: Unsigned long integer
+ * @m: Unsigned long integer
+ *
+ * Return: The number of bits to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor_result = n ^ m, count = 0;
+
+	while (xor != 0)
+	{
+		count += xor & 1;
+		xor_result >>= 1;
+	}
+
+	return (count);
+}
