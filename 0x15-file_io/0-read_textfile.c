0c6565ca1372ce347afefd70a9e9092dc7894c21
edake <102189081+edake419@users.noreply.github.com>
2023-08-07 21:43:47 +0000

    read_textfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters written. It fails, returns 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_name;
+	ssize_t Read, Write;
+	char *buffer;
+
+	if (filename ==NULL)
+		return (0);
+
+	file_name = open(filename, O_RDONLY);
+
+	if (file_name == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * (letters));
+	if (buffer == null)
+		return (0);
+
+	Read = read(file_name, buffer, letters);
+	if (Read == -1)
+	{
+		free(buffer);
+		close(file_name);
+		return (0);
+	}
+	d
+	Write = write(STDOUT_FILENO, buffer, Read);
+	if (Write == -1)
+	{
+		free(buffer);
+		close(file_name);
+		return (0);
+	}
+	
+	close(file_name);
+
+	return (Write);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,2 @@
+
+file io project, read, write and append files
