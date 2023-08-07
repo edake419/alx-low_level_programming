4290c23933ee40d660c1ac5364a89bc756f5b4ea
edake <102189081+edake419@users.noreply.github.com>
2023-08-07 22:38:57 +0000

    created

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file, file_write, 
+	int file, file_write, t_length=0; 
 
 	if (filename == NULL)
 		return (-1);
@@ -20,7 +20,9 @@ int create_file(const char *filename, char *text_content)
 
     if (text_content != NULL)
     {
-	    file_write = write(file, text_content, strlen(text_content));
+	    while (text_content[t_length])
+		countt++;
+		file_write = write(file, text_content, t_length);
         if (file_write == -1)
 	    {
 		    close(file);
